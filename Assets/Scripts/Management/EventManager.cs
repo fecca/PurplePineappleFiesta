45f241fa0fafd645253a711a2958a0532db1997d
fecca <niklas.cronfalk@gmail.com>
2018-09-19 20:23:47 +0200

    Item events.

--- Assets/Scripts/Management/EventManager.cs
+++ Assets/Scripts/Management/EventManager.cs
@@ -6,6 +6,7 @@ public class EventManager : MonoBehaviour
 	private void Awake()
 	{
 		SetupGenericEvents();
+		SetupItemEvents();
 	}
 
 	#region GENERIC EVENTS
@@ -23,6 +24,25 @@ public class EventManager : MonoBehaviour
 	public static void TriggerEvent(GenericEventType type)
 	{
 		m_genericEvents[type].Raise();
+	}
+
+	#endregion
+
+	#region ITEM EVENTS
+
+	[SerializeField]
+	private ItemEvent PickedUpEvent;
+
+	private static Dictionary<ItemEventType, ItemEvent> m_itemEvents = new Dictionary<ItemEventType, ItemEvent>();
+
+	private void SetupItemEvents()
+	{
+		m_itemEvents.Add(ItemEventType.PickedUp, PickedUpEvent);
+	}
+
+	public static void TriggerEvent(ItemEventType type, Item item)
+	{
+		m_itemEvents[type].Raise(item);
 	}
 
 	#endregion
---
+++ Assets/Scripts/Utility/ExtensionMethods.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public static class ExtensionMethods
+{
+	public static Component GetOrAddComponent<T>(this GameObject go) where T : Component
+	{
+		var comp = go.GetComponent<T>();
+		if (comp == null)
+		{
+			comp = go.AddComponent<T>();
+		}
+		return comp;
+	}
+}
