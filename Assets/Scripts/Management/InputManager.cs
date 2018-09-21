9f34d8b172b73f0e4c349421b908cdda011c3b7b
Niklas Erlandsson <niklas.erlandsson@cortopia.com>
2018-09-21 15:12:54 +0200

    Shotgun.

--- Assets/Scripts/Management/InputManager.cs
+++ Assets/Scripts/Management/InputManager.cs
@@ -2,26 +2,64 @@
 
 public class InputManager : MonoBehaviour
 {
+	private float m_updateInterval = 0.2f;
+	private float m_timer;
+
 	private void Update()
 	{
-		if (Input.GetMouseButtonUp(0))
+		if (Input.GetMouseButtonDown(0))
 		{
+			m_timer = 0f;
+
 			RaycastHit hit;
 			var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 			if (Physics.Raycast(ray, out hit, 100f))
 			{
-				var item = hit.collider.GetComponent<Item>();
-				if (item != null)
-				{
-					EventManager.TriggerEvent(ItemEventType.Clicked, item);
-					return;
-				}
-
 				if (hit.collider.tag.Equals("Ground"))
 				{
 					EventManager.TriggerEvent(WorldEventType.ClickedGround, hit.point);
 				}
 			}
+		}
+
+		if (Input.GetMouseButton(0))
+		{
+			if (m_timer >= m_updateInterval)
+			{
+				m_timer = 0f;
+
+				RaycastHit hit;
+				var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+				if (Physics.Raycast(ray, out hit, 100f))
+				{
+					if (hit.collider.tag.Equals("Ground"))
+					{
+						EventManager.TriggerEvent(WorldEventType.ClickedGround, hit.point);
+					}
+				}
+			}
+
+			m_timer += Time.deltaTime;
+		}
+
+		if (Input.GetMouseButtonUp(0))
+		{
+			if (m_timer <= 0.1f)
+			{
+				RaycastHit hit;
+				var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+				if (Physics.Raycast(ray, out hit, 100f))
+				{
+					var item = hit.collider.GetComponent<Item>();
+					if (item != null)
+					{
+						EventManager.TriggerEvent(ItemEventType.Clicked, item);
+						return;
+					}
+				}
+			}
+
+			m_timer = 0f;
 		}
 
 		if (Input.GetKeyDown(KeyCode.Space))
--- Assets/Scripts/Player/Weapon.cs
+++ Assets/Scripts/Player/Weapon.cs
@@ -10,18 +10,42 @@ public class Weapon : MonoBehaviour
 	private int m_bullets;
 	[SerializeField]
 	private float m_power;
+	[SerializeField]
+	private float m_spread;
+	[SerializeField]
+	private Material m_material;
+	[SerializeField]
+	private GameObject m_lineRenderer;
+
+	//private void Update()
+	//{
+	//if (Input.GetKeyDown(KeyCode.Space))
+	//{
+	//	Shoot();
+	//}
+	//}
 
 	public void Shoot()
 	{
 		for (var i = 0; i < m_bullets; i++)
 		{
-			var bullet = Instantiate(m_bullet);
-			bullet.transform.position = m_barrel.position;
-			var rigidbody = bullet.GetOrAddComponent<Rigidbody>();
-			var direction = m_barrel.forward + (Vector3.right * (Random.value - 0.5f)) + (Vector3.up * (Random.value - 0.5f));
-			rigidbody.AddForce(direction * m_power);
+			var direction = m_barrel.forward + (Vector3.right * (Random.value - 0.5f) * m_spread) + (Vector3.up * (Random.value - 0.5f) * m_spread);
+			RaycastHit hitInfo;
+			if (Physics.Raycast(new Ray(m_barrel.position, direction), out hitInfo, 100f))
+			{
+				var lrgo = Instantiate(m_lineRenderer);
+				var lr = lrgo.GetComponent<LineRenderer>();
+				lr.SetPositions(new Vector3[] { m_barrel.position, hitInfo.point });
+				Destroy(lrgo, 0.1f);
+			}
 
-			Destroy(bullet, 5.0f);
+			//var bullet = Instantiate(m_bullet);
+			//bullet.transform.position = m_barrel.position;
+			//var rigidbody = bullet.GetOrAddComponent<Rigidbody>();
+			//var direction = m_barrel.forward + (Vector3.right * (Random.value - 0.5f)) + (Vector3.up * (Random.value - 0.5f));
+			//rigidbody.AddForce(direction * m_power);
+
+			//Destroy(bullet, 5.0f);
 		}
 	}
 }
