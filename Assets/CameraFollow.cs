6ed4534d3c1de0dffdf9636db93537bff6876b76
fecca <niklas.cronfalk@gmail.com>
2018-09-28 20:47:21 +0200

    Camera rotation.

--- Assets/CameraFollow.cs
+++ Assets/CameraFollow.cs
@@ -2,13 +2,29 @@
 
 public class CameraFollow : MonoBehaviour
 {
+	[Header("Position")]
 	[SerializeField]
 	private Transform m_parent;
 	[SerializeField]
 	private float m_followSpeed;
 
+	[Header("Rotation")]
+	[SerializeField]
+	private bool m_rotate;
+	[SerializeField]
+	private float m_rotationSpeed;
+
 	private void Update()
 	{
 		transform.position = Vector3.Lerp(transform.position, m_parent.position, Time.deltaTime * m_followSpeed);
+
+		if (m_rotate)
+		{
+			if (Input.GetMouseButton(1))
+			{
+				var mouseX = Input.GetAxis("Mouse X");
+				transform.RotateAround(m_parent.position, Vector3.up, mouseX * Time.deltaTime * m_rotationSpeed);
+			}
+		}
 	}
 }
